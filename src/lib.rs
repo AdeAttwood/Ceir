f9846f242ac15c190868d0016c8968216ebd9793
Ade Attwood <code@adeattwood.co.uk>
2024-12-28 15:14:57 +0000

    chore: remove the old src impl of the engine
    This has now been split into separate packages. All of the chess logic has been
    move into the `common` package. The engine is in the engine package.
    
    This is still a very dumb engine. I have still yet to get a win with it.

--- src/lib.rs
+++
@@ -1,8 +0,0 @@
-pub mod bitboards;
-pub mod board;
-pub mod fen;
-pub mod move_gen;
-pub mod moves;
-pub mod search;
-pub mod uci;
-pub mod uci_command;
--- src/pgn/mod.rs
+++
@@ -1,4 +0,0 @@
-pub mod game;
-pub mod game_result;
-pub mod parser;
-pub mod reader;
